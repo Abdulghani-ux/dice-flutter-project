e345a708ece28ee6daa9c18993df67ad30959397
nasir4440 <abdulghanihusein@gmail.com>
2022-10-06 11:04:41 +0300

    fun project

---
+++ android/app/src/main/kotlin/com/example/dice_roject_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dice_roject_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: Container(),
+      ),
+    ),
+  );
+}
