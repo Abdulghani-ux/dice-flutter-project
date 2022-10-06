4bc8053463750e8f20ffbeb257fdb8f62970a341
nasir4440 <abdulghanihusein@gmail.com>
2022-10-06 11:16:32 +0300

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,54 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: Container(),
+        body: DicePage(),
       ),
     ),
   );
 }
+
+class DicePage extends StatefulWidget {
+  const DicePage({key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber= 1;
+  int rightDiceNumber=1;
+  void changedDiceFace(){
+    setState(() {
+      leftDiceNumber= Random().nextInt(6)+1;
+      rightDiceNumber=Random().nextInt(6)+1;
+      print('left dice number = $leftDiceNumber');
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(child: TextButton(
+            onPressed: (){
+              changedDiceFace();
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'
+            ),
+          ),
+          ),
+          Expanded(child: TextButton(
+            onPressed: (){
+              changedDiceFace();
+            },
+            child: Image.asset('images/dice$rightDiceNumber.png'),
+          ),
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
+
